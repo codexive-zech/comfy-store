50f3baf4fb102eb34812bf4e2a047c0553a215af
Codexive_zech <zechariahhounwanou@gmail.com>
2022-05-16 16:14:32 +0100

    added the url for all products to be fetched and single product to be fetched from the server, also implemented a func that check if the element (class or id) exist in the HTML element or not

---
+++ src/utils.js
@@ -0,0 +1,38 @@
+//   ATTENTION!!!!!!!!!!!
+//   I SWITCHED TO PERMANENT DOMAIN
+//   DATA IS THE SAME JUST A DIFFERENT URL,
+//   DOES NOT AFFECT PROJECT FUNCTIONALITY
+
+// url to fetch all products to display
+const allProductsUrl = "https://course-api.com/javascript-store-products";
+// temporary single product
+// 'https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog'
+// url to fetch just single product
+const singleProductUrl =
+  "https://course-api.com/javascript-store-single-product";
+
+//   a func to get each elements classes and id to see if it's valid
+const getElement = (selection) => {
+  // targeting the element that is going to be selected to take in action and effect
+  const element = document.querySelector(selection);
+  //   checking to see if the element does exist in the HTML as class or id
+  if (element) return element;
+  //   when the element class or id does not exist in the HTML then an error should be thrown
+  throw new Error(
+    `Please check "${selection}" selector, no such element exist`
+  );
+};
+
+const formatPrice = () => {};
+
+const getStorageItem = () => {};
+const setStorageItem = () => {};
+
+export {
+  allProductsUrl,
+  singleProductUrl,
+  getElement,
+  formatPrice,
+  getStorageItem,
+  setStorageItem,
+};
