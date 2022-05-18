83cbbab6c7b11bdb63b7dad5cb10736805d3a23b
Codexive_zech <zechariahhounwanou@gmail.com>
2022-05-18 21:05:14 +0100

    imported specific functionality (store, displayProduct & getElement) needed to display all the store items in the product container element

--- src/pages/products.js
+++ src/pages/products.js
@@ -2,3 +2,11 @@
 import "../toggleSidebar.js";
 import "../cart/toggleCart.js";
 import "../cart/setupCart.js";
+
+// Specific import to display store already available
+import { store } from "../store.js";
+import displayProducts from "../displayProducts.js";
+import { getElement } from "../utils.js";
+
+// getting and displaying the store array available into the products container (parent element for all product)
+displayProducts(store, getElement(".products-container"));
