61cbe8abc91b049d7372ad13a2b470d22397c1b5
Codexive_zech <zechariahhounwanou@gmail.com>
2022-05-20 21:27:43 +0100

    Added the functionality to set products into the cart
    get products from the cart local storage which is an empty array and then an init func

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -1,7 +1,24 @@
+// importing global functionality
+import { getElement, getStorageItem, setStorageItem } from "../utils.js";
 // importing the functionality that opens up the cart side bar when the cart button is clicked
 import { openCart } from "./toggleCart.js";
+
+// selecting elements that will take in effects
+const cartItemCountDOM = getElement(".cart-item-count");
+const cartItemsDOM = getElement(".cart-items");
+const cartTotalDOM = getElement(".cart-total");
+
+// getting the initial product from the cart local storage which is currently empty (haven't been set)
+let cart = getStorageItem("cart");
+// a functionality that add products to the cart via the id available in the store local storage
 export const addToCart = (id) => {
   console.log(id);
   //   adding the open cart functionality so as to show the cart and it's item when the button is clicked clicked
   openCart();
 };
+
+// init declare
+const init = () => {
+  console.log("hel");
+};
+init();
