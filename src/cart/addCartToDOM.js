1b3c02d1560b9a89a1ab81432be6a89ef211707c
Codexive_zech <zechariahhounwanou@gmail.com>
2022-05-21 15:18:33 +0100

    added comments fro understanding to the add cart to dom functionality

--- src/cart/addCartToDOM.js
+++ src/cart/addCartToDOM.js
@@ -1,9 +1,16 @@
+// imported the needed function module
 import { formatPrice, getElement } from "../utils.js";
+// selecting element that will take in effect
 const cartItems = getElement(".cart-items");
+// a functionality that add product to cart and also destructured the needed properties from the object
 const addCartToDOM = ({ id, name, amount, price, image }) => {
+  // create a new element name article
   const article = document.createElement("article");
+  // adding a class to the created
   article.classList.add("cart-item");
+  // setting attributes to the created element
   article.setAttribute("data-id", id);
+  // dynamically inserting the HTML into the new created article element
   article.innerHTML = `
             <img
               src="${image}"
@@ -26,6 +33,8 @@ const addCartToDOM = ({ id, name, amount, price, image }) => {
                 <i class="fa fa-chevron-down"></i>
               </button>
             </div>`;
+  // adding / appending the newly created element to it's parent element
   cartItems.appendChild(article);
 };
+// exporting this functionality to other module
 export default addCartToDOM;
